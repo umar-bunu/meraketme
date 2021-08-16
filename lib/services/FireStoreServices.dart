05eb612ced3ef0efdeaec03984641f056b00b5b8
umar-bunu <umar.bunu73@gmail.com>
2021-08-16 16:13:39 +0300

    stable update with notifications working

--- lib/services/FireStoreServices.dart
+++ lib/services/FireStoreServices.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class FireStoreServices {
   Future getShippingDetails(user) async {
@@ -15,14 +16,27 @@ class FireStoreServices {
 
   Future getOrderHistory(user) async {
     try {
+      debugPrint('19');
       var _query = await FirebaseFirestore.instance
           .collection('orderHistory')
-          .where('email', isEqualTo: user)
+          .where('customer', isEqualTo: user)
           .get();
-      List _docs =
-          _query.docs.map((e) => {'id': e.id, 'data': e.data()}).toList();
+      List _docs = [];
+
+      for (var element in _query.docs) {
+        var _query2 = await FirebaseFirestore.instance
+            .collection('foods')
+            .doc(element.data()['item'])
+            .get();
+        var _mappedResult = _query2.data();
+        _mappedResult!.addAll(element.data());
+        _docs.add({'id': element.id, 'data': _mappedResult});
+      }
+      debugPrint(_docs.toString());
+
       return _docs;
     } on FirebaseException catch (e) {
+      debugPrint(e.toString());
       return Future.error(e.code);
     }
   }
